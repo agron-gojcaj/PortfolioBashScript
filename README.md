5f5093097a68abdd7c85e7f681e6efbfd6819ef8
agron-gojcaj <agron.gojcaj@365smartshop.com>
2024-07-02 17:18:04 -0400

    Made Bash calculator

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# PortfolioBashScript
+## PortfolioBashScript
+
+Welcome to my Bash Calculator!
+
+This Bash Calculator will allow you to perform addition, subtraction, multiplication, and integer division
+on any 2 numbers of your choosing.
+
+Note: When doing division, Bash performs integer division, meaning the remainder of the quotient is truncated, or removed.
+For example, 7 / 4 = 1.75, but in this calculator, 7 / 4 = 1.
---
+++ portfolio_bash_script.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+clear
+
+name="Agron Gojcaj"
+sum=0
+status="y"
+
+echo "Hello, welcome to $name's Bash calculator!"
+echo "Enter one number: "
+read n1
+echo "Enter a second number: "
+read n2
+while [ $status = "y" ]; do
+	echo "1.Addition"
+	echo "2.Subtraction"
+	echo "3.Multiplication"
+	echo "4.Division"
+	echo "Enter your choice"
+	read operation
+	case $operation in
+	1)
+		sum=$(expr $n1 + $n2)
+		echo "$n1 + $n2 = "$sum
+		;;
+	2)
+		sum=$(expr $n1 - $n2)
+		echo "$n1 - $n2 = "$sum
+		;;
+	3)
+		sum=$(expr $n1 \* $n2)
+		echo "$num1 * $num2 = "$sum
+		;;
+	4)
+		sum=$(expr $n1 / $n2)
+		echo "$n1 / $n2 = "$sum
+		;;
+	*) echo "Invalid choice" ;;
+	esac
+	echo "Do u want to continue (y/n)) ?"
+	read status
+	if [ $status != "y" ]; then
+		exit
+	fi
+done
